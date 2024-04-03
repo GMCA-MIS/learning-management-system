9423f16bda74affdf7cc3f6c81a212dbb7bb06cb
AISHITERU20 <165883610+AISHITERU20@users.noreply.github.com>
2024-04-03 16:43:20 +0800

    Update admin_name.php

--- admin/includes/admin_name.php
+++ admin/includes/admin_name.php
@@ -18,13 +18,12 @@ while ($row = mysqli_fetch_array($admin_user)) {
                       <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Welcome, <?php echo $firstname . ' ' . $lastname; ?></span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Welcome! Admin :  <?php echo $firstname . ' ' . $lastname; ?></span>
                                 <img class="img-profile rounded-circle" src="<?php echo $location ?>">
                             </a>
 
                             <!-- Dropdown menu -->
                             <div class="dropdown-menu" aria-labelledby="userDropdown">
                                 <a class="dropdown-item" href="admin_profile.php"><i class="fa fa-user" aria-hidden="true"></i> Profile</a>
-                                <a class="dropdown-item" href="change_password.php"> <i class="fa fa-lock" aria-hidden="true"></i> Change Password</a>
                             </div>
                         </li>
