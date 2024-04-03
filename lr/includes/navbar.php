c1660d13d514757734b1f723fa6555b922074ddd
AISHITERU20 <165883610+AISHITERU20@users.noreply.github.com>
2024-04-03 19:41:19 +0800

    Update navbar.php

--- lr/includes/navbar.php
+++ lr/includes/navbar.php
@@ -2,6 +2,7 @@
 <ul class="navbar-nav new-nav-bg sidebar sidebar-dark accordion" id="accordionSidebar">
 
 <!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center"
     <div class="sidebar-brand-icon">
         <img src="../img/gmlogo.png" alt="">
     </div>
