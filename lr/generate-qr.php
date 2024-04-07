3f2d45b14db42a71f2d507874c084e471a3de27a
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-07 14:18:40 +0800

    LR - LCard [Update]
    Button for navigation

--- lr/generate-qr.php
+++ lr/generate-qr.php
@@ -36,3 +36,22 @@ include('dbcon.php');
 
         </nav>
         <!-- End of Topbar -->
+
+        <!--Buttons-->
+        <div class="row mb-5">
+          <div class="btn-group">
+            <button type="button" class="btn btn-success add_btn" data-toggle="modal" data-target="#add_lcardModal">
+                <i class="fa fa-plus" aria-hidden="true"></i> Add New Card
+            </button>
+            <a href="generate-id.php">
+                <button type="button" class="btn btn-success add_btn" data-toggle="modal" data-target="#add_lcardModal">
+                    <i class="fa fa-plus" aria-hidden="true"></i> Generate ID Card
+                </button>
+            </a>
+            <a href="generate-qr.php">
+                <button type="button" class="btn btn-success add_btn" data-toggle="modal" data-target="#add_lcardModal">
+                    <i class="fa fa-plus" aria-hidden="true"></i> Generate QR Code
+                </button>
+            </a>
+          </div>
+        </div>
