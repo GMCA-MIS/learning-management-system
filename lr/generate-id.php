b34941bed18a2a4fbd6951eca0d232c5001154b4
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-07 17:44:02 +0800

    Update generate-id.php

--- lr/generate-id.php
+++ lr/generate-id.php
@@ -52,113 +52,146 @@
         </nav>
         <!-- End of Topbar -->
 
-        <div></div>
-
-        <button type="button" class="btn btn-success add_btn" data-toggle="modal" data-target="#add_lcardkModal">
-            <i class="fa fa-plus" aria-hidden="true"></i> Add New Card
-        </button>
-        <a href="generate-id.php">
-            <button type="button" class="btn btn-success add_btn" data-toggle="modal" data-target="#gen_lcardModal">
-                <i class="fa fa-address-card" aria-hidden="true"></i> Generate ID Card
-            </button>
-        </a>
-        <a href="generate-qr.php">
-            <button type="button" class="btn btn-success add_btn" data-toggle="modal" data-target="#gen_qrModal">  
-                <i class="fa fa-qrcode" aria-hidden="true"></i> Generate QR Code
-            </button>
-        </a>
-
-        <!-- Add New Card Modal -->
-        <div class="modal fade" id="add_lcardkModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">New ID Card
-                        </h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-
-                    <div class="container">
-                        <form method="POST" enctype="multipart/form-data">
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <label for="name">Full Name</label>
-                                    <input type="text" class="form-control" placeholder="Full name" name="name">
-                                </div>
-                                <div class="col-sm-4">
-                                    <label for="department">Department</label>
-                                    <select name="dept" class="custom-select custom-select">
-                                        <option selected></option>
-                                        <option value="Computer Studies">Computer Studies</option>
-                                        <option value="Education">Education</option>
-                                        <option value="Agriculture">Agriculture</option>
-                                        <option value="Junior High School">Junior High School</option>
-                                        <option value="Senior High School">Senior High School</option>
-                                    </select>
-                                </div>
-                                <div class="col-sm-4">
-                                    <label for="category">Category</label>
-                                    <select name="cat" class="custom-select custom-select">
-                                        <option selected></option>
-                                        <option value="Student">Student</option>
-                                        <option value="Faculty">Faculty</option>
-                                        <option value="Admin">Admin</option>
-                                    </select>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <label for="address">Address</label>
-                                    <input type="text" class="form-control" placeholder="Type your address" name="address">
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <label for="email">Email</label>
-                                    <input type="text" class="form-control" placeholder="Your email" name="email">
-                                </div>
-                                <div class="col-sm-6">
-                                    <label for="birthdate">Birthdate</label>
-                                    <input type="date" class="form-control" id="birthdate" name="dob">
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <label for="id">ID Card No.</label>
-                                    <input type="text" class="form-control" placeholder="ID No." name="id_no">
-                                </div>
-                                <div class="col-sm-6">
-                                    <label for="contact">Contact No.</label>
-                                    <input type="text" class="form-control" placeholder="Phone number" name="phone">
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <label for="photo">Upload Photo</label>
-                                    <input type="file" class="form-control" name="image">
-                                </div>
-                            </div>
-
-                            <!-- Modal footer -->
-                            <div class="modal-footer">
-                                <button type="submit" class="btn btn-warning">Submit</button>
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            </div>
-                        </form>
-                    </div>
-
-                </div>
-                <!--modal content -->
+        <!-- Begin Page Content -->
+        <div class="modal-body">
+            <!-- <ul class="nav nav-tabs" id="assignmentTabs" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="createTab" data-toggle="tab" href="#createAssignment" role="tab"
+                        aria-controls="createAssignment" aria-selected="true"></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " id="uploadTab" data-toggle="tab" href="#uploadAssignment" role="tab"
+                        aria-controls="uploadAssignment" aria-selected="false"></a>
+                </li>
+            </ul>
+            <hr> -->
+            <div></div>
+            <div class="tab-content" id="assignmentTabsContent">
+                <div class="tab-pane fade" id="uploadAssignment" role="tabpanel" aria-labelledby="uploadTab">
             </div>
-            <!--modal dialog -->
+            <div class="tab-pane fade show active" id="createAssignment" role="tabpanel" aria-labelledby="createTab">
+                <div class="card-body">
+
+                    <div class="table-responsive">
+
+                        <td>
+                            <!--Add Pop Up Modal -->
+                            <div class="modal fade" id="add_lcardkModal" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">New ID Card
+                                            </h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+
+                                        <div class="container">
+                                            <form method="POST" enctype="multipart/form-data">
+                                                <div class="row">
+                                                    <div class="col-sm-4">
+                                                        <label for="name">Full Name</label>
+                                                        <input type="text" class="form-control" placeholder="Full name" name="name">
+                                                    </div>
+                                                    <div class="col-sm-4">
+                                                        <label for="department">Department</label>
+                                                        <select name="dept" class="custom-select custom-select">
+                                                            <option selected></option>
+                                                            <option value="Computer Studies">Computer Studies</option>
+                                                            <option value="Education">Education</option>
+                                                            <option value="Agriculture">Agriculture</option>
+                                                            <option value="Junior High School">Junior High School</option>
+                                                            <option value="Senior High School">Senior High School</option>
+                                                        </select>
+                                                    </div>
+                                                    <div class="col-sm-4">
+                                                        <label for="category">Category</label>
+                                                        <select name="cat" class="custom-select custom-select">
+                                                            <option selected></option>
+                                                            <option value="Student">Student</option>
+                                                            <option value="Faculty">Faculty</option>
+                                                            <option value="Admin">Admin</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col-sm-12">
+                                                        <label for="address">Address</label>
+                                                        <input type="text" class="form-control" placeholder="Type your address" name="address">
+                                                    </div>
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col-sm-6">
+                                                        <label for="email">Email</label>
+                                                        <input type="text" class="form-control" placeholder="Your email" name="email">
+                                                    </div>
+                                                    <div class="col-sm-6">
+                                                        <label for="birthdate">Birthdate</label>
+                                                        <input type="date" class="form-control" id="birthdate" name="dob">
+                                                    </div>
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col-sm-6">
+                                                        <label for="id">ID Card No.</label>
+                                                        <input type="text" class="form-control" placeholder="ID No." name="id_no">
+                                                    </div>
+                                                    <div class="col-sm-6">
+                                                        <label for="contact">Contact No.</label>
+                                                        <input type="text" class="form-control" placeholder="Phone number" name="phone">
+                                                    </div>
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col-sm-12">
+                                                        <label for="photo">Upload Photo</label>
+                                                        <input type="file" class="form-control" name="image">
+                                                    </div>
+                                                </div>
+
+                                                <!-- Modal footer -->
+                                                <div class="modal-footer">
+                                                <button type="submit" class="btn btn-warning">Submit</button>
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                </div>
+                                            </form>
+                                        </div>
+
+                                    </div>
+                                    <!--modal content -->
+                                </div>
+                                <!--modal dialog -->
+                            </div>
+                            <!--modal fade -->
+                            <button type="button" class="btn btn-success add_btn" data-toggle="modal" data-target="#add_lcardkModal">
+                                <i class="fa fa-plus" aria-hidden="true"></i> Add New Card
+                            </button>
+                            <a href="generate-id.php">
+                                <button type="button" class="btn btn-success add_btn" data-toggle="modal" data-target="#gen_lcardModal">
+                                    <i class="fa fa-address-card" aria-hidden="true"></i> Generate ID Card
+                                </button>
+                            </a>
+                            <a href="generate-qr.php">
+                                <button type="button" class="btn btn-success add_btn" data-toggle="modal" data-target="#gen_qrModal">  
+                                    <i class="fa fa-qrcode" aria-hidden="true"></i> Generate QR Code
+                                </button>
+                            </a>
+                        </td>
+
+                        <div class="d-sm-flex align-items-center justify-content-between mb-2"
+                            style="margin-top: 10px; margin-left: 10px;">
+                            <h1 class="h5 mb-0 text-gray-800"></h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <!--modal fade -->
+
+
 
 <?php
     include('includes/scripts.php');
