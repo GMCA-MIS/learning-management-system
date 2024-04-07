8269a1de7733ee9841463ee106d62f1db0f3a07a
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-07 17:02:43 +0800

    Update manage-id-card.php

--- lr/manage-id-card.php
+++ lr/manage-id-card.php
@@ -196,77 +196,80 @@
                                                 </button>
                                             </div>
 
-                                            <form method="POST" enctype="multipart/form-data">
-                                                <div class="row">
-                                                
-                                                    <div class="col">
-                                                        <label for="name">Full Name</label>
-                                                        <input type="text" class="form-control" placeholder="Full name" name="name">
+                                            <div class="container">
+                                                <form method="POST" enctype="multipart/form-data">
+                                                    <div class="row">
+                                                    
+                                                        <div class="col">
+                                                            <label for="name">Full Name</label>
+                                                            <input type="text" class="form-control" placeholder="Full name" name="name">
+                                                        </div>
+                                                        <div class="col">
+                                                            <label for="department">Department</label>
+                                                            <select name="dept" class="custom-select custom-select">
+                                                                <option selected></option>
+                                                                <option value="Computer Studies">Computer Studies</option>
+                                                                <option value="Education">Education</option>
+                                                                <option value="Agriculture">Agriculture</option>
+                                                                <option value="Junior High School">Junior High School</option>
+                                                                <option value="Senior High School">Senior High School</option>
+                                                            </select>
+                                                        </div>
+                                                        <div class="col">
+                                                            <label for="category">Category</label>
+                                                            <select name="cat" class="custom-select custom-select">
+                                                                <option selected></option>
+                                                                <option value="Student">Student</option>
+                                                                <option value="Faculty">Faculty</option>
+                                                                <option value="Admin">Admin</option>
+                                                            </select>
+                                                        </div>
                                                     </div>
-                                                    <div class="col">
-                                                        <label for="department">Department</label>
-                                                        <select name="dept" class="custom-select custom-select">
-                                                            <option selected></option>
-                                                            <option value="Computer Studies">Computer Studies</option>
-                                                            <option value="Education">Education</option>
-                                                            <option value="Agriculture">Agriculture</option>
-                                                            <option value="Junior High School">Junior High School</option>
-                                                            <option value="Senior High School">Senior High School</option>
-                                                        </select>
-                                                    </div>
-                                                    <div class="col">
-                                                        <label for="category">Category</label>
-                                                        <select name="cat" class="custom-select custom-select">
-                                                            <option selected></option>
-                                                            <option value="Student">Student</option>
-                                                            <option value="Faculty">Faculty</option>
-                                                            <option value="Admin">Admin</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
 
-                                                <div class="row">
-                                                    <div class="col">
-                                                        <label for="address">Address</label>
-                                                        <input type="text" class="form-control" placeholder="Type your address" name="address">
+                                                    <div class="row">
+                                                        <div class="col">
+                                                            <label for="address">Address</label>
+                                                            <input type="text" class="form-control" placeholder="Type your address" name="address">
+                                                        </div>
                                                     </div>
-                                                </div>
 
-                                                <div class="row">
-                                                    <div class="col">
-                                                        <label for="email">Email</label>
-                                                        <input type="text" class="form-control" placeholder="Your email" name="email">
+                                                    <div class="row">
+                                                        <div class="col">
+                                                            <label for="email">Email</label>
+                                                            <input type="text" class="form-control" placeholder="Your email" name="email">
+                                                        </div>
+                                                        <div class="col">
+                                                            <label for="birthdate">Birthdate</label>
+                                                            <input type="date" class="form-control" id="birthdate" name="dob">
+                                                        </div>
                                                     </div>
-                                                    <div class="col">
-                                                        <label for="birthdate">Birthdate</label>
-                                                        <input type="date" class="form-control" id="birthdate" name="dob">
-                                                    </div>
-                                                </div>
 
-                                                <div class="row">
-                                                    <div class="col">
-                                                        <label for="id">ID Card No.</label>
-                                                        <input type="text" class="form-control" placeholder="ID No." name="id_no">
+                                                    <div class="row">
+                                                        <div class="col">
+                                                            <label for="id">ID Card No.</label>
+                                                            <input type="text" class="form-control" placeholder="ID No." name="id_no">
+                                                        </div>
+                                                        <div class="col">
+                                                            <label for="contact">Contact No.</label>
+                                                            <input type="text" class="form-control" placeholder="Phone number" name="phone">
+                                                        </div>
                                                     </div>
-                                                    <div class="col">
-                                                        <label for="contact">Contact No.</label>
-                                                        <input type="text" class="form-control" placeholder="Phone number" name="phone">
-                                                    </div>
-                                                </div>
 
-                                                <div class="row">
-                                                    <div class="col">
-                                                        <label for="photo">Upload Photo</label>
-                                                        <input type="file" class="form-control" name="image">
+                                                    <div class="row">
+                                                        <div class="col">
+                                                            <label for="photo">Upload Photo</label>
+                                                            <input type="file" class="form-control" name="image">
+                                                        </div>
                                                     </div>
-                                                </div>
 
-                                                <!-- Modal footer -->
-                                                <div class="modal-footer">
-                                                <button type="submit" class="btn btn-warning">Submit</button>
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                </div>
-                                            </form>
+                                                    <!-- Modal footer -->
+                                                    <div class="modal-footer">
+                                                    <button type="submit" class="btn btn-warning">Submit</button>
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                            
                                         </div>
                                         <!--modal content -->
                                     </div>
