8d46f77945c471000e9dfa2c23c6b5bd0915fee5
Rhoshielamie Libuna <rhoshielamielibuna@outlook.com>
2024-04-09 08:55:58 +0800

    generate qr [update]

--- lr/generate-qr.php
+++ lr/generate-qr.php
@@ -205,7 +205,9 @@
                 <div class="col-6">
                     <label for="generate">Student ID Card No.</label>
                     <form autocomplete="off" action="generate-qr.php" method="post">
-                        <input type="text" class="form-control col-6" value="<?php echo $first_name;?>" name="first_name" placeholder="Type your ID no. here"><br>
+                        <input type="text" class="form-control col-6" value="<?php echo $first_name;?>" name="first_name" placeholder="Type your ID no. here">
+                        <br>
+                        <br>
                         <button type="submit" class="btn btn-warning" name="generate"><span class="fa fa-cog mr-3"></span>Generate</button>
                     </form>
                 </div>
