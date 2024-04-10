766d8748c76cdf7e2eb19bdc472338000fcc2812
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-11 03:07:05 +0800

    Update qr-scanner.php

--- lr/qr-scanner.php
+++ lr/qr-scanner.php
@@ -88,8 +88,96 @@
                ";
           unset($_SESSION['success']);
         }
-        
+
         ?>
+
+          <!--Data Table-->
+          <div class="table-striped">
+            <table id="example1" class="table table-striped table-bordered" style="width:100%">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>User's ID</th>
+                    <th>Time In</th>
+                    <th>Time Out</th>
+                    <th>Logdate</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <!--Table value from 'attendance' and 'student'-->
+                <?php
+                
+                $server = "srv1320.hstgr.io";
+                $username="u944705315_capstone2024";
+                $password="Capstone@2024.";
+                $dbname="u944705315_capstone2024";
+                
+
+                $conn = new mysqli($server,$username,$password,$dbname);
+                $date = date('Y-m-d');
+
+                  if($conn->connect_error){
+                  die("Connection failed" .$conn->connect_error);
+                }
+
+                $sql ="SELECT * FROM attendance_lr LEFT JOIN student ON attendance_lr.stud_no=cards.stud_no WHERE LOGDATE='$date'";
+                $query = $conn->query($sql);
+                  while ($row = $query->fetch_assoc()){
+                ?>
+                    <tr>
+                      <td><?php echo $row['firstname'];?></td>
+                      <td><?php echo $row['stud_id'];?></td>
+                      <td><?php echo $row['timein'];?></td>
+                      <td><?php echo $row['timeout'];?></td>
+                      <td><?php echo $row['logdate'];?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+
+                <!--Camera Scanner Script-->
+                <script>
+                let scanner = new Instascan.Scanner({ video: document.getElementById('preview')});
+                Instascan.Camera.getCameras().then(function(cameras){
+                    if(cameras.length > 0 ){
+                        scanner.start(cameras[0]);
+                    } else{
+                        alert('No cameras found');
+                    }
+
+                }).catch(function(e) {
+                    console.error(e);
+                });
+
+                scanner.addListener('scan',function(c){
+                    document.getElementById('text').value=c;
+                    document.forms[0].submit();
+                });
+                </script>
+
+                <!--Time-->
+                <script type="text/javascript">
+                    date_default_timezone_set('Asia/Manila');
+                var timestamp = '<?=time();?>';
+                function updateTime(){
+                $('#time').html(Date(timestamp));
+                timestamp++;
+                }
+                $(function(){
+                setInterval(updateTime, 1000);
+                });
+                </script>
+
+
+                <script>
+                new DataTable('#example1');
+                </script>
+
+            </tbody>
+            </table>
+          </div>
+    </div>
 
 
 <?php
