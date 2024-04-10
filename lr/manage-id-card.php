cad36af26479fa261e3c7d5235cb316fb618db28
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-10 10:52:29 +0800

    Update manage-id-card.php

--- lr/manage-id-card.php
+++ lr/manage-id-card.php
@@ -20,6 +20,112 @@
     }
 </style>
 
+<!--Code for queries-->
+<?php 
+
+    $insert = false;
+    $update = false;
+    $empty = false;
+    $delete = false;
+    $already_card = false;
+
+
+    if(isset($_GET['delete'])) {
+
+    $stud_no = $_GET['delete'];
+    $delete = true;
+    $sql = "DELETE FROM `cards` WHERE `stud_no` = $stud_no";
+    $result = mysqli_query($conn, $sql);
+
+    }
+      
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset( $_POST['stud_no_edit'])){
+      
+
+    //Update the records
+    $stud_no = $_POST["stud_no_edit"];
+    $name = $_POST["edit_name"];
+    $id_no = $_POST["id_no_edit"];
+      
+
+    //SQL query
+    $sql = "UPDATE `cards` SET `name` = '$name' , `id_no` = '$id_no' WHERE `cards`.`stud_no` = $stud_no";
+    $result = mysqli_query($conn, $sql);
+    
+    if($result) {
+    
+        $update = true;
+
+    }
+
+    else {
+        echo "Sorry, we could not update your record.";
+
+    }
+    }
+      
+    else {
+      
+        $name = $_POST["name"];
+        $dept = $_POST["dept"];
+        $cat = $_POST["cat"];
+        $address = $_POST["address"];
+        $email = $_POST["email"];
+        $dob = $_POST["dob"];
+        $id_no = $_POST["id_no"];
+        $phone = $_POST["phone"];
+      
+    if($name == '' || $id_no == '') {
+
+        $empty = true;
+
+    }
+      
+    else {
+              
+    //Check if the card no. is already registered or not
+    $querry = mysqli_query($conn, "SELECT * FROM cards WHERE id_no= '$id_no' ");
+            
+        if(mysqli_num_rows($querry)>0)
+        {
+        $already_card = true;
+        }
+    
+        else {
+      
+        // Image upload 
+        $uploaddir = 'assets/uploads/';
+        $uploadfile = $uploaddir . basename($_FILES['image']['name']);
+    
+        if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
+        } 
+
+        else {
+            echo "Possible file upload attack!\n";
+        }
+      
+    // Sql query to be executed
+    $sql = "INSERT INTO `cards`(`name`, `dept`, `cat`, `address`,`email`, `dob`, `id_no`, `phone`, `image`) VALUES ('$name','$dept','$cat','$address','$email','$dob','$id_no', '$phone', '$uploadfile')"; 
+      
+    // $sql = "INSERT INTO `cards` (`name`, `id_no`) VALUES ('$name', '$id_no')";
+    $result = mysqli_query($conn, $sql);
+         
+        if($result) { 
+            $insert = true;
+        }
+
+        else {
+            echo "The record was not inserted successfully because of this error ---> ". mysqli_error($conn);
+    } 
+    }
+    }
+    }
+    }
+
+?>
+<!--End of query-->
+
 <!-- Content Wrapper -->
 <div id="content-wrapper" class="d-flex flex-column">
 
@@ -51,6 +157,69 @@
 
         </nav>
         <!-- End of Topbar -->
+
+
+        <!--Alert Messages-->
+        <!--Insert-->
+        <?php
+        if($insert){
+        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+                <strong>Success!</strong> Your card has been inserted successfully.
+                
+                <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>×</span>
+                </button>
+              </div>";
+        }
+        ?>
+        <!--Delete-->
+        <?php
+        if($delete){
+        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+                <strong>Success!</strong> Your card has been deleted successfully.
+                    
+                <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>×</span>
+                </button>
+              </div>";
+        }
+        ?>
+        <!--Update-->
+        <?php
+        if($update){
+        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+                <strong>Success!</strong> Your Card has been updated successfully
+                    
+                <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>×</span>
+                </button>
+              </div>";
+        }
+        ?>
+        <!--If Empty-->
+        <?php
+        if($empty){
+        echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                <strong>Error!</strong> The Fields Cannot Be Empty! Please Give Some Values.
+                    
+                <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>×</span>
+                </button>
+              </div>";
+        }
+        ?>
+        <!--If already have a card-->
+        <?php
+        if($already_card){
+        echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                <strong>Error!</strong> This Card is Already Added.
+                    
+                <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>×</span>
+                </button>
+              </div>";
+        }
+        ?>
 
         <!-- Begin Page Content -->
         <div class="modal-body">
@@ -297,6 +466,7 @@
                                 style="margin-top: 10px; margin-left: 10px;">
                                 <h1 class="h5 mb-0 text-gray-800"></h1>
                             </div>
+                            
                             <?php
                             // Displaying data into tables
                             $query = "SELECT booklist.*, category.category_name
