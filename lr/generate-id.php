1b906799460e9c58b1b3a6acf15b1cf4b925559d
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-10 17:07:39 +0800

    Update generate-id.php

--- lr/generate-id.php
+++ lr/generate-id.php
@@ -322,8 +322,21 @@ $html.="
             </div>
         </div>
 
-<div class="row"></div>
 <?php
     include('includes/scripts.php');
-    include('includes/footer.php');
 ?>
+
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span> &copy; Golden Minds Colleges and Academy</span>
+                    </div>
+                </div>
+</footer>
+            <!-- End of Footer -->
+
+        <!-- End of Content Wrapper -->
+</body>
+
+</html>
