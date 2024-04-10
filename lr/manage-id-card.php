3fa368701b358dc1ded0bb8d88afc9ec0889fb48
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-10 12:08:56 +0800

    Update manage-id-card.php

--- lr/manage-id-card.php
+++ lr/manage-id-card.php
@@ -504,46 +504,6 @@
                                                   </tr>";
                                         } 
                                         ?>
-                                                
-
-                                                <!-- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
-                                                <td>
-                                                    <!--Edit Pop Up Modal -->
-                                                    <div class="modal fade" id="edit_bookModal" tabindex="-1" role="dialog"
-                                                        aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                        <div class="modal-dialog" role="document">
-                                                            <div class="modal-content">
-                                                                <div class="modal-header">
-                                                                    <h5 class="modal-title" id="exampleModalLabel">Edit Book
-                                                                        Information
-                                                                    </h5>
-                                                                </div>
-                                                                <form method="POST">
-                                                                    <div class="modal-body">
-                                                                        <input type="hidden" name="snoEdit" id="snoEdit">
-                                                                        <div class="form-group">
-                                                                            <label for="name">Student Name</label>
-                                                                            <input type="text" class="form-control" id="nameEdit" name="nameEdit">
-                                                                        </div>
-
-                                                                        <div class="form-group">
-                                                                            <label for="desc">ID Card Number:</label>
-                                                                            <input class="form-control" id="id_noEdit" name="id_noEdit" rows="3"></input>
-                                                                        </div> 
-                                                                    </div>
-                                                                    <div class="modal-footer d-block mr-auto">
-                                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                                        <button type="submit" class="btn btn-primary">Save changes</button>
-                                                                    </div>
-                                                                </form>
-                                                            </div> <!--modal content -->
-                                                        </div> <!--modal dialog -->
-                                                    </div> <!--modal fade -->
-
-
-                                                    <button type="submit" name="edit_btn"
-                                                        class="btn btn-success edit_btn">Edit</button>
-                                                </td>
                 
                         </tbody>
                         </table>
