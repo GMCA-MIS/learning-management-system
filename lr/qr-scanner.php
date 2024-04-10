e5ed824cc4445d2906ee3f014cc1e2be2a8d73cd
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-11 00:47:05 +0800

    Update qr-scanner.php

--- lr/qr-scanner.php
+++ lr/qr-scanner.php
@@ -4,3 +4,25 @@
     include('includes/navbar.php');
     include('dbcon.php');
 ?>
+
+<style>
+    a {
+        text-decoration: none;
+        /* Remove underline to the anchor text */
+        color: black;
+        /* Set the text color to blue */
+    }
+
+    a:hover {
+        color: brown;
+    }
+    label {
+        margin-top: 5%;
+    }
+</style>
+
+
+<?php
+    include('includes/scripts.php');
+    include('includes/footer.php');
+?>
