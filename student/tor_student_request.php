4977e2cf4cc76a1c26d502209004983b9149664d
Rey <80256611+rey-a-reyes@users.noreply.github.com>
2024-04-13 21:53:34 +0800

    Update tor_student_request.php

--- student/tor_student_request.php
+++ student/tor_student_request.php
@@ -16,6 +16,14 @@ include('initialize.php');
 
         <!-- Topbar -->
 
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top">
+
+            <!-- Sidebar Toggle (Topbar) -->
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                <i class="fa fa-bars"></i>
+            </button>
+        </nav>
+
         <!-- End of Topbar -->
 
         <!-- Begin Page Content -->
