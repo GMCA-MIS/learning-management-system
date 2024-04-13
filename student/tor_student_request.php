7392152986d7756ca8392e0537d1092624fcc79e
Rey <80256611+rey-a-reyes@users.noreply.github.com>
2024-04-13 21:52:11 +0800

    Update tor_student_request.php

--- student/tor_student_request.php
+++ student/tor_student_request.php
@@ -16,22 +16,6 @@ include('initialize.php');
 
         <!-- Topbar -->
 
-	<!--
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-            <!-- Sidebar Toggle (Topbar) -->
-            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                <i class="fa fa-bars"></i>
-            </button>
-            
-            <!-- Page Heading -->
-             
-		<div class="d-sm-flex align-items-center justify-content-between mb-4" style="margin-top: 27px; margin-left: 10px;">
-                <h1 class="h3 mb-0 text-gray-800">Profile</h1>
-            	</div>
-        </nav>
-
-	-->
         <!-- End of Topbar -->
 
         <!-- Begin Page Content -->
