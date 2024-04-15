115abc491b64e8f4e4be3426b3b05641121d0f54
Red Star <80256611+redcaloocan@users.noreply.github.com>
2024-04-15 23:09:07 +0800

    changed password showing error
    changed password showing error.
    
    root cause connection for database is wrong

--- passwordreset/dbconn.php
+++ passwordreset/dbconn.php
@@ -1,7 +1,11 @@
 <?php
 // dbcon.php
 
-$mysqli = new mysqli('localhost','root','','capstone');
+
+
+
+
+$mysqli = new mysqli('srv1320.hstgr.io','u944705315_capstone2024','Capstone@2024','u944705315_capstone2024');
 
 if ($mysqli->connect_error) {
     die("Database connection failed: " . $mysqli->connect_error);
