d988cee56d62349dab431b5f6288ade799c99bd6
Red Star <80256611+redcaloocan@users.noreply.github.com>
2024-04-15 23:23:00 +0800

    changed password wrong credentials
    changed password wrong credentials

--- passwordreset/dbconn.php
+++ passwordreset/dbconn.php
@@ -5,7 +5,7 @@
 
 
 
-$mysqli = new mysqli('srv1320.hstgr.io','u944705315_capstone2024','Capstone@2024','u944705315_capstone2024');
+$mysqli = new mysqli('srv1320.hstgr.io','u944705315_capstone2024','Capstone@2024.','u944705315_capstone2024');
 
 if ($mysqli->connect_error) {
     die("Database connection failed: " . $mysqli->connect_error);
