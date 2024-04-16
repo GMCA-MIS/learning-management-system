03fdfb2a97c4aa01b04a328ad45df9c883016ef9
Red Star <80256611+redcaloocan@users.noreply.github.com>
2024-04-16 20:09:07 +0800

    landing page announcement

--- landingpage/parse_content.php
+++ landingpage/parse_content.php
@@ -6,7 +6,6 @@ if($_POST['content'] == "mission"){
 
     $query = "SELECT * FROM content  where title = 'Mission'";
     $query_run = mysqli_query($conn, $query);
-    $missions = array();
 
     if (mysqli_num_rows($query_run) > 0) {
         while ($row = mysqli_fetch_assoc($query_run)) {
@@ -19,7 +18,6 @@ if($_POST['content'] == "mission"){
 
     $query = "SELECT * FROM content  where title = 'Vision'";
     $query_run = mysqli_query($conn, $query);
-    $missions = array();
 
     if (mysqli_num_rows($query_run) > 0) {
         while ($row = mysqli_fetch_assoc($query_run)) {
@@ -29,7 +27,6 @@ if($_POST['content'] == "mission"){
 }elseif($_POST['content'] == "upcomingevent"){
     $query = "SELECT * FROM content  where title = 'Upcoming Events'";
     $query_run = mysqli_query($conn, $query);
-    $missions = array();
 
     if (mysqli_num_rows($query_run) > 0) {
         while ($row = mysqli_fetch_assoc($query_run)) {
@@ -40,7 +37,6 @@ if($_POST['content'] == "mission"){
 
     $query = "SELECT * FROM content  where title = 'Announcements'";
     $query_run = mysqli_query($conn, $query);
-    $missions = array();
 
     if (mysqli_num_rows($query_run) > 0) {
         while ($row = mysqli_fetch_assoc($query_run)) {
