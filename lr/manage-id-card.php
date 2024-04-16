c837f2eb8437f75d838e47a9e6bf3c15ff16da0f
Red Star <80256611+redcaloocan@users.noreply.github.com>
2024-04-16 22:36:27 +0800

    add manage id and edit

--- lr/manage-id-card.php
+++ lr/manage-id-card.php
@@ -85,7 +85,7 @@
     else {
               
     //Check if the card no. is already registered or not
-    $querry = mysqli_query($conn, "SELECT * FROM cards WHERE id_no= '$id_no' ");
+    $querry = mysqli_query($conn, "SELECT * FROM cards WHERE stud_no= '$id_no' ");
             
         if(mysqli_num_rows($querry)>0)
         {
@@ -95,7 +95,7 @@
         else {
       
         // Image upload 
-        $uploaddir = 'lr/img';
+        $uploaddir = 'img';
         $uploadfile = $uploaddir . basename($_FILES['image']['name']);
     
         if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
@@ -106,7 +106,7 @@
         }
       
     // Sql query to be executed
-    $sql = "INSERT INTO `cards`(`name`, `dept`, `cat`, `address`,`email`, `dob`, `id_no`, `phone`, `image`) VALUES ('$name','$dept','$cat','$address','$email','$dob','$id_no', '$phone', '$uploadfile')"; 
+    $sql = "INSERT INTO `cards`(`name`, `dept`, `cat`, `address`,`email`, `dob`, `stud_no`, `phone`, `image`) VALUES ('$name','$dept','$cat','$address','$email','$dob','$id_no', '$phone', '$uploadfile')"; 
       
     // $sql = "INSERT INTO `cards` (`name`, `id_no`) VALUES ('$name', '$id_no')";
     $result = mysqli_query($conn, $sql);
@@ -491,12 +491,138 @@
                                                     <td>". $row['name'] . "</td>
                                                     <td>". $row['stud_no'] . "</td>
                                                     <td>
-                                                        <button class='edit btn btn-sm btn-success col-5' id=".$row['stud_no'].">Edit</button>
+                                                        <button class='edit btn btn-sm btn-success  col-5' data-toggle='modal' data-target='#editcard".$row['stud_no']."' id=".$row['stud_no'].">Edit</button>
                                                         <button class='delete btn btn-sm btn-danger col-5' id=d".$row['stud_no'].">Delete</button></td>
                                                   </tr>";
-                                        } 
+                                        
                                         ?>
-                
+
+                                        <!--Add Pop Up Modal -->
+                                <div class="modal fade" id="<?php echo "editcard". $row['stud_no']; ?>" tabindex="-1" role="dialog"
+                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">Update ID Card
+                                                </h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+
+                                            <div class="container">
+                                                <form method="POST" enctype="multipart/form-data" action="id-card-update-function.php">
+                                                    <div class="row">
+                                                        <div class="col-sm-4">
+                                                            <input type="text" value="<?php echo $row['stud_no'] ?>" name="updatecard" hidden>
+                                                            <input type="text" value="<?php echo $row['stud_no'] ?>" name="stud_no" hidden>
+                                                            <label for="name">Full Name</label>
+                                                            <input type="text" class="form-control" value="<?php echo $row['name']; ?>" placeholder="Full name" name="name">
+                                                        </div>
+                                                        <div class="col-sm-4">
+                                                            <label for="department">Department</label>
+                                                            <select name="dept" class="custom-select custom-select">
+
+
+                                                                <option value="<?php echo $row['dept'] ;?>" selected><?php echo $row['dept'] ;?></option>
+                                                                <?php if($row['dept'] != 'Computer Studies'){ ?>
+                                                                <option value="Computer Studies">Computer Studies</option>
+                                                                <?php }
+                                                                    if($row['dept'] != 'Education'){
+                                                                ?>
+                                                                <option value="Education">Education</option>
+                                                                <?php }
+                                                                    if($row['dept'] != 'Agriculture'){
+                                                                ?>
+                                                                <option value="Agriculture">Agriculture</option>
+                                                                <?php }
+                                                                    if($row['dept'] != 'Junior High School'){
+                                                                ?>
+                                                                <option value="Junior High School">Junior High School</option>
+                                                                <?php }
+                                                                    if($row['dept'] != 'Senior High School'){
+                                                                ?>
+                                                                <option value="Senior High School">Senior High School</option>
+                                                                <?php }
+                                                                ?>
+
+
+                                                            </select>
+                                                        </div>
+                                                        <div class="col-sm-4">
+                                                            <label for="category">Category</label>
+                                                            <select name="cat" class="custom-select custom-select">
+
+                                                                <option value="<?php echo $row['cat']; ?>" Selected><?php echo $row['cat']; ?></option>
+                                                                <?php if($row['cat'] != 'Student'){ ?>
+                                                                <option value="Student">Student</option>
+                                                                <?php }
+                                                                    if($row['cat'] != 'Faculty'){
+                                                                ?>
+                                                                <option value="Faculty">Faculty</option>
+                                                                <?php }
+                                                                    if($row['cat'] != 'Admin'){
+                                                                ?>
+                                                                <option value="Admin">Admin</option>
+                                                                <?php }
+                                                                ?>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="row">
+                                                        <div class="col-sm-12">
+                                                            <label for="address">Address</label>
+                                                            <input type="text" class="form-control" placeholder="Type your address" name="address" value="<?php echo $row['address']; ?>">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="row">
+                                                        <div class="col-sm-6">
+                                                            <label for="email">Email</label>
+                                                            <input type="text" class="form-control" placeholder="Your email" name="email" value="<?php echo $row['email']; ?>">
+                                                        </div>
+                                                        <div class="col-sm-6">
+                                                            <label for="birthdate">Birthdate</label>
+                                                            <input type="date" class="form-control" id="birthdate" name="dob" value="<?php echo $row['dob']; ?>">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="row">
+                                                        <div class="col-sm-6">
+                                                            <label for="id">ID Card No.</label>
+                                                            <input type="text" class="form-control" placeholder="ID No." name="id_no"  value="<?php echo $row['stud_no']; ?>">
+                                                        </div>
+                                                        <div class="col-sm-6">
+                                                            <label for="contact">Contact No.</label>
+                                                            <input type="text" class="form-control" placeholder="Phone number" name="phone"  value="<?php echo $row['phone']; ?>">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="row">
+                                                        <div class="col-sm-12">
+                                                            <label for="photo">Upload Photo</label>
+                                                            <input type="file" class="form-control" name="image">
+                                                        </div>
+                                                    </div>
+
+                                                    <!-- Modal footer -->
+                                                    <div class="modal-footer">
+                                                    <button type="submit" class="btn btn-warning">Submit</button>
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+
+                                        </div>
+                                        <!--modal content -->
+                                    </div>
+                                    <!--modal dialog -->
+                                </div>
+                                <!--modal fade -->
+                                    <?php 
+                                            } 
+                                ?>
                         </tbody>
                         </table>
                     </div>
