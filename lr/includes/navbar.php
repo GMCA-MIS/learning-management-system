431435c14409b1cbd8cf45cf22aaae85fdb6c66e
Rhoshielamie Libuna <rhoshielamielibuna@outlook.com>
2024-04-16 11:50:25 +0800

    Update navbar.php
    for e-lib (ui)

--- lr/includes/navbar.php
+++ lr/includes/navbar.php
@@ -72,7 +72,7 @@
       <div class="bg-white py-2 collapse-inner rounded">
         <h6 class="collapse-header">Manage Library Card</h6>
         <a class="collapse-item" href="manage-id-card.php?id=<?php echo $school_year ?>"><i class='fa-solid fa-id-card-clip'></i> Manage Library Cards</a>
-        <a class="collapse-item" href="generate-id.php?id=<?php echo $school_year ?>"><i class="fa-solid fa-id-card-clip" aria-hidden="true"></i> ID Generator</a>
+        <a class="collapse-item" href="generate-id.php?id=<?php echo $school_year ?>"><i class="fa-solid fa-id-card"></i> ID Generator</a>
         <a class="collapse-item" href="generate-qr.php?id=<?php echo $school_year ?>"><i class="fa fa-qrcode" aria-hidden="true"></i> QR Generator</a>
       </div>
     </div>
