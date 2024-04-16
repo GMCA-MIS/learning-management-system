5ede9b7e4bdd39dbb2e860bd76818f203b1bebf1
Red Star <80256611+redcaloocan@users.noreply.github.com>
2024-04-16 18:44:17 +0800

    vision and mission is now coded and showing in  landingpage

---
+++ landingpage/parse_content.php
@@ -0,0 +1,29 @@
+<?php
+include('dbcon.php');
+
+
+if($_POST['content'] == "mission"){
+
+    $query = "SELECT * FROM content  where title = 'Mission'";
+    $query_run = mysqli_query($conn, $query);
+    $missions = array();
+
+    if (mysqli_num_rows($query_run) > 0) {
+        while ($row = mysqli_fetch_assoc($query_run)) {
+           echo $row['content'];
+        }
+    }
+
+    
+}elseif($_POST['content'] == "vision"){
+
+    $query = "SELECT * FROM content  where title = 'Vision'";
+    $query_run = mysqli_query($conn, $query);
+    $missions = array();
+
+    if (mysqli_num_rows($query_run) > 0) {
+        while ($row = mysqli_fetch_assoc($query_run)) {
+           echo $row['content'];
+        }
+    }
+}
