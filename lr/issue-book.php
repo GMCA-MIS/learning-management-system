d4a7417018c579dfc09d5bf065da679f38e65ad5
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-17 20:09:18 +0800

    Update issue-book.php

--- lr/issue-book.php
+++ lr/issue-book.php
@@ -44,7 +44,7 @@
             <!-- Page Heading -->
             <div class="d-sm-flex align-items-center justify-content-between mb-4"
                 style="margin-top: 27px; margin-left: 10px;">
-                <h1 class="h3 mb-0 text-gray-800">Book Issuance</h1>
+                <h1 class="h3 mb-0 text-gray-800">Book Borrowing</h1>
             </div>
 
             <!-- Topbar Navbar -->
