94c64a2638520c4afc35f3434cc8c2cd5440fe26
Red Star <80256611+redcaloocan@users.noreply.github.com>
2024-04-18 11:06:20 +0800

    Update process-reset-password.php

--- passwordreset/process-reset-password.php
+++ passwordreset/process-reset-password.php
@@ -8,6 +8,9 @@
     <link rel="stylesheet" type="text/css" href="css/header.css?version=<?php echo time(); ?>">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+
 </head>
 <body>
 
