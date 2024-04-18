8e611c4011281019d7dc5e8a7465a00854796893
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-18 16:45:52 +0800

    Update issue-book.php

--- lr/issue-book.php
+++ lr/issue-book.php
@@ -69,7 +69,8 @@
                 <div class="input-group mb-3">
                     <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
                     <div class="input-group-append">
-                        <span class="input-group-text" id="basic-addon2">@example.com</span>
+                        <button class="btn btn-outline-secondary" type="button">Button</button>
+                        <button class="btn btn-outline-secondary" type="button">Button</button>
                     </div>
                 </div>
                 <!--<div class="card">
