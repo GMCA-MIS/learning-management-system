f72241290bf07c3ccfbed1ecf61a2f3b35658c48
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-18 16:47:52 +0800

    Update issue-book.php

--- lr/issue-book.php
+++ lr/issue-book.php
@@ -69,8 +69,8 @@
                 <div class="input-group mb-3">
                     <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
                     <div class="input-group-append">
-                        <button class="btn btn-warning" type="button">Issue</button>
-                        <button class="btn btn-success" type="button">Return</button>
+                        <button class="btn" type="button">Issue</button>
+                        <button class="btn" type="button">Return</button>
                     </div>
                 </div>
                 <!--<div class="card">
