0f5534f737c10454a3ff7ddb73ee4b76c02db87d
Red Star <80256611+redcaloocan@users.noreply.github.com>
2024-04-18 11:04:31 +0800

    Update process-reset-password.php

--- passwordreset/process-reset-password.php
+++ passwordreset/process-reset-password.php
@@ -1,4 +1,16 @@
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+<!DOCTYPE html>
+<html lang ="en">
+
+<head>
+    <meta charset="UTF-8">
+	<title> Password Reset </title>
+    <meta name="viewport" content="width = device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/header.css?version=<?php echo time(); ?>">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+
 <?php
 $token = $_POST["token"];
 $token_hash = hash("sha256", $token);
@@ -113,3 +125,5 @@ if ($userCoordinator !== null) {
     echo "<script>alert('Password updated. You can now login to your account.'); window.location.href = '../newlogin.php';</script>";
 }
 ?>
+</body>
+</html>
