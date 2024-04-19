20acbf8caabe0df8e7c1adce422a1538be38f1cf
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-20 03:27:35 +0800

    Update issue-book.php

--- lr/issue-book.php
+++ lr/issue-book.php
@@ -121,7 +121,42 @@
                                 title: 'Scanned successfully.',
                                 html: '<input class='swal2-input id='book_id' placeholder='Enter student Number'',
                                 icon: 'success'
-                                confirmButtonText: 'Issue'
+                                confirmButtonText: 'Issue'Swal.fire({
+                                    title: "Submit your Github username",
+                                    input: "text",
+                                    inputAttributes: {
+                                      autocapitalize: "off"
+                                    },
+                                    showCancelButton: true,
+                                    confirmButtonText: "Look up",
+                                    showLoaderOnConfirm: true,
+                                    preConfirm: async (login) => {
+                                      try {
+                                        const githubUrl = `
+                                          https://api.github.com/users/${login}
+                                        `;
+                                        const response = await fetch(githubUrl);
+                                        if (!response.ok) {
+                                          return Swal.showValidationMessage(`
+                                            ${JSON.stringify(await response.json())}
+                                          `);
+                                        }
+                                        return response.json();
+                                      } catch (error) {
+                                        Swal.showValidationMessage(`
+                                          Request failed: ${error}
+                                        `);
+                                      }
+                                    },
+                                    allowOutsideClick: () => !Swal.isLoading()
+                                  }).then((result) => {
+                                    if (result.isConfirmed) {
+                                      Swal.fire({
+                                        title: `${result.value.login}'s avatar`,
+                                        imageUrl: result.value.avatar_url
+                                      });
+                                    }
+                                  });
                             })
                           </script>";
 
