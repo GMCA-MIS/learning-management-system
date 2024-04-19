a894b748511143bdde351c5e2d0502a4339c45db
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-20 03:33:13 +0800

    Update issue-book.php

--- lr/issue-book.php
+++ lr/issue-book.php
@@ -119,7 +119,7 @@
                     echo "<script type='text/javascript'>
                             Swal.fire({
                                 title: 'Scanned successfully.',
-                                html: '<input class='swal2-input id='book_id' placeholder='Enter student no.'',
+                                text: 'Something went wrong!',
                                 icon: 'success'
                                 confirmButtonText: 'Issue'
                             })
