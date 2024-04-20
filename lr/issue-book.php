1a3126cbb1468e0a99383843d5c91c95fb38fee3
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-21 04:35:55 +0800

    Update issue-book.php

--- lr/issue-book.php
+++ lr/issue-book.php
@@ -150,7 +150,7 @@ crossorigin="anonymous" referrerpolicy="no-referrer" />
                                 <td><?php echo $row['book_id']; ?></td>
                                 <td><?php echo $row['book_title']; ?></td>
                                 <td class="justify-content-between mr-1">
-                                    <button class="btn btn-danger" type="button" id="borrow" name="borrow">
+                                    <button class="btn btn-warning" type="button" id="borrow" name="borrow">
                                         <i class="fa-solid fa-hand"></i>
                                     </button>
                                     <button class="btn btn-info" type="button" id="view-deets" name="view-deets">
