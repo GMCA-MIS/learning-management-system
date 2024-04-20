48449fbaa80f3b76f39798e31dbddfa46ac5952a
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-21 04:26:42 +0800

    Update issue-book.php

--- lr/issue-book.php
+++ lr/issue-book.php
@@ -13,6 +13,11 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.8/dist/sweetalert2.all.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.8/dist/sweetalert2.min.css">
 
+<!--Font Awesome-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" 
+integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" 
+crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 <style>
     a {
         text-decoration: none;
