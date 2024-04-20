ff553a73308ab05a3974c556b57ad3b143114010
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-21 04:47:05 +0800

    Update issue-book.php

--- lr/issue-book.php
+++ lr/issue-book.php
@@ -150,7 +150,7 @@ crossorigin="anonymous" referrerpolicy="no-referrer" />
                                 <td><?php echo $row['book_id']; ?></td>
                                 <td><?php echo $row['book_title']; ?></td>
                                 <td>
-                                    <button class="btn btn-warning mr-3" type="button" id="borrow" name="borrow">
+                                    <button class="btn btn-warning mr-3" type="button" id="borrow" name="borrow" data-bs-toggle="modal" data-bs-target="#borrow-book">
                                         <i class="fa-solid fa-hand"></i>
                                     </button>
                                     <button class="btn btn-info mr-3" type="button" id="view-deets" name="view-deets">
@@ -168,6 +168,26 @@ crossorigin="anonymous" referrerpolicy="no-referrer" />
                 }
                 
                 ?>
+
+
+        <!-- Modal -->
+        <div class="modal fade" id="borrow-book" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Understood</button>
+            </div>
+            </div>
+        </div>
+        </div>
     
                 
             
