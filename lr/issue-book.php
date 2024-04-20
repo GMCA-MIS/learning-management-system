4c1b145e0d76a7b98dfce6d2bcbbfc6bcff9cc27
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-21 05:45:21 +0800

    Update issue-book.php

--- lr/issue-book.php
+++ lr/issue-book.php
@@ -180,8 +180,8 @@ crossorigin="anonymous" referrerpolicy="no-referrer" />
         </script>
 
         <!-- Modal for Borrow button-->
-        <div class="modal fade" id="borrow_book" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal fade" role="dialog" id="borrow_book" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog" role="dialog">
             <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
