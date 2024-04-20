7d99d46c9324aa51e72d3b59bef1f369880b500d
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-20 13:45:15 +0800

    Update issue-book.php

--- lr/issue-book.php
+++ lr/issue-book.php
@@ -121,7 +121,7 @@
                                 title: 'Book scanned successfully!',
                                 text: 'You clicked the button!',
                                 html:
-                                '<input>',
+                                '<input id='stud_no' placeholder='Enter Stud No. Here'>',
                                 icon: 'success'
                             });
                         
