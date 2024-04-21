fdb08f9b058c335e314093300eb4a264f6b3c0e8
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-22 02:52:12 +0800

    Update issue-book.php

--- lr/issue-book.php
+++ lr/issue-book.php
@@ -181,7 +181,7 @@ crossorigin="anonymous" referrerpolicy="no-referrer" />
                     //Check student number if it exists in student table
 
 
-                    if(isset($_POST['issue'])) {
+                    if(isset($_POST['stud_no'])) {
                         $borrower = $_POST['stud_no'];
 
                         $sql ="SELECT * FROM student WHERE student_id='$borrower'";
