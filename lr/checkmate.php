48365f177059444613cc86da0890a25bb9238bfc
dethrone13 <165910002+dethrone13@users.noreply.github.com>
2024-04-23 01:13:06 +0800

    Update checkmate.php

--- lr/checkmate.php
+++ lr/checkmate.php
@@ -1,54 +1,7 @@
 <?php
     session_start();
 
-?>            
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Modal Popup Example</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-<body>
-
-<!-- Button to Open the Modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-  Open modal
-</button>
-
-<!-- The Modal -->
-<div class="modal" id="myModal">
-  <div class="modal-dialog">
-    <div class="modal-content">
-
-      <!-- Modal Header -->
-      <div class="modal-header">
-        <h4 class="modal-title">Insert Data</h4>
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-      </div>
-
-      <!-- Modal Body -->
-      <div class="modal-body">
-        <form id="insertForm" action="checkmate.php" method="POST">
-          <div class="form-group">
-            <label for="data">Data:</label>
-            <input type="text" class="form-control" id="data" name="data">
-          </div>
-          <input type="submit" class="btn btn-primary"></input>
-        </form>
-      </div>
-
-    </div>
-  </div>
-</div>
-
-<?php
-
+	
 $server = "srv1320.hstgr.io";
 $username="u944705315_capstone2024";
 $password="Capstone@2024.";
@@ -96,6 +49,54 @@ $(document).ready(function(){
   });
 });
 </script>
+
+?>            
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Modal Popup Example</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<!-- Button to Open the Modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+  Open modal
+</button>
+
+<!-- The Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Insert Data</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal Body -->
+      <div class="modal-body">
+        <form id="insertForm" action="checkmate.php" method="POST">
+          <div class="form-group">
+            <label for="data">Data:</label>
+            <input type="text" class="form-control" id="data" name="data">
+          </div>
+          <input type="submit" class="btn btn-primary"></input>
+        </form>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+
 
 
 
