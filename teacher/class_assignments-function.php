8cbdb181f2d7199ff5d564f03a5b951066ea5fa3
Red Star <80256611+redcaloocan@users.noreply.github.com>
2024-04-22 16:57:40 +0800

    Update class_assignments-function.php

--- teacher/class_assignments-function.php
+++ teacher/class_assignments-function.php
@@ -71,7 +71,7 @@ if (!$atLeastOneFileUploaded) {
 
 // Insert the assignment into the database with file locations as a JSON array
 $fileLocationsJson = json_encode($fileLocations);
-$name_notification = 'Add Assignment file name titled:' . ' <b>' . $name . '</b>';
+$name_notification = 'Add Assignment titled:' . ' <b>' . $name . '</b>';
 $date_of_notification = date('Y-m-d H:i:s'); // Get the current date and time
 
 
