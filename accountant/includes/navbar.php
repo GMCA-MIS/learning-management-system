336b74b48399b060b9f8b1a42f394e47c337f8e2
Red Star <80256611+redcaloocan@users.noreply.github.com>
2024-05-06 00:38:42 +0800

    push

--- accountant/includes/navbar.php
+++ accountant/includes/navbar.php
@@ -1,7 +1,7 @@
 <!-- Sidebar -->
 <ul class="navbar-nav new-nav-bg sidebar sidebar-dark accordion" id="accordionSidebar">
   <!-- Sidebar - Brand -->
-  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../admin/index.php">
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
     <div class="sidebar-brand-icon">
       <img src="img/gmlogo.png" alt="">
     </div>
@@ -36,66 +36,6 @@
   $school_year = $school_year_query_row['school_year_id'];
   ?>
 
-
-  <!-- <li class="nav-item <?php if (basename($_SERVER['PHP_SELF']) == 'manage-students.php') echo 'active'; ?>">
-  <a class="nav-link" href="manage-students.php">
-    <i class="fas fa-book-reader" aria-hidden="true"></i>
-    <span>Manage Students</span>
-  </a>
-</li> -->
-
-
-
-  <!-- <li class="nav-item <?php if (basename($_SERVER['PHP_SELF']) == 'manage-departments.php') echo 'active'; ?>">
-  <a class="nav-link" href="manage-departments.php">
-  <i class='far fa-building'></i>
-    <span>Departments</span>
-  </a>
-</li> -->
-
-  <!-- <li class="nav-item <?php if (basename($_SERVER['PHP_SELF']) == 'manage-class.php') echo 'active'; ?>">
-  <a class="nav-link" href="manage-class.php">
-  <i class='fas fa-chalkboard'></i>
-    <span>Sections</span>
-  </a>
-</li> -->
-
-  <!-- <li class="nav-item <?php if (basename($_SERVER['PHP_SELF']) == 'manage-courses.php') echo 'active'; ?>">
-  <a class="nav-link" href="manage-courses.php">
-  <i class='fas fa-book-open'></i>
-    <span>Manage Subjects</span>
-  </a>
-</li>
-
-<li class="nav-item <?php if (basename($_SERVER['PHP_SELF']) == 'manage-assigncourses.php') echo 'active'; ?>">
-  <a class="nav-link" href="manage-assigncourses.php">
-    <i class="fas fa-book-reader" aria-hidden="true"></i>
-    <span>Assign Subjects</span>
-  </a>
-</li> -->
-
-  <!-- <li class="nav-item <?php if (basename($_SERVER['PHP_SELF']) == 'manage-teachers.php') echo 'active'; ?>">
-  <a class="nav-link" href="manage-teachers.php">
-  <i class="fas fa-chalkboard-teacher"></i>
-    <span>Manage Teachers</span>
-  </a>
-</li> -->
-
-  <!-- 
-  <li class="nav-item <?php if (in_array(basename($_SERVER['PHP_SELF']), ['manage-departments.php', 'manage-teachers.php'])) echo 'active'; ?>">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapses" aria-expanded="true" aria-controls="collapseTwo">
-      <i class="fas fa-chalkboard-teacher"></i>
-      <span>Manage Teachers</span>
-    </a>
-    <div id="collapses" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-      <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Manage Teachers</h6>
-        <a class="collapse-item" href="manage-departments.php?id=<?php echo $school_year ?>"> <i class='far fa-building'></i> Department List</a>
-        <a class="collapse-item" href="manage-teachers.php?id=<?php echo $school_year ?>"> <i class="fas fa-chalkboard-teacher"></i> Teacher List </a>
-      </div>
-    </div>
-  </li>
-   -->
 
   <!-- Nav Item - Pages Collapse Menu -->
   <li class="nav-item <?php if (in_array(basename($_SERVER['PHP_SELF']), ['manage-students-enlisted.php', 'manage-students.php','manage-stud-transcation.php'])) echo 'active'; ?>">
@@ -144,27 +84,6 @@
     </div>
   </li>
 
-
-  <!--  -->
-
-  <!-- Nav Item - Pages Collapse Menu 
-  <li class="nav-item <?php if (in_array(basename($_SERVER['PHP_SELF']), ['sectors.php', 'offices.php', 'process.php'])) echo 'active'; ?>">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-      <i class='fas fa-school'></i>
-      <span>School</span>
-    </a>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-      <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Select Classification</h6>
-        <a class="collapse-item" href="calendar.php?id=<?php echo $school_year ?>"><i class='far fa-calendar'></i> School Calendar</a>
-        <a class="collapse-item" href="schoolyear.php?id=<?php echo $school_year ?>"><i class='fas fa-graduation-cap'></i> School Year</a>
-        <a class="collapse-item" href="content.php?id=<?php echo $school_year ?>"><i class="fa fa-edit"></i> Website Content</a>
-      </div>
-    </div>
-  </li>
--->
-
-
   <!-- Sidebar Toggler (Sidebar) -->
   <div class="text-center d-none d-md-inline">
     <button class="rounded-circle border-0" id="sidebarToggle"></button>
@@ -173,18 +92,11 @@
   <hr class="sidebar-divider">
 
   <li class="nav-item">
-    <a class="nav-link" href="logout.php" data-toggle="modal" data-target="#logoutModal" style="font-size:13.6px;">
+    <a class="nav-link"  data-toggle="modal" data-target="#logoutModal" style="font-size:13.6px;">
       <i class="fa fa-power-off" aria-hidden="true"></i>
       Logout
     </a>
   </li>
-
-  <!--<li class="nav-item">
-  <a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">
-  <i class="fa fa-power-off" aria-hidden="true"></i>
-    <span>Logout</span>
-  </a>
-</li>-->
 
 
 
