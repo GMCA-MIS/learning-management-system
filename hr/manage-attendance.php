1a63fbb4b73d48202215c66841c0bc8d9ef4427b
Red Star <80256611+redcaloocan@users.noreply.github.com>
2024-05-08 04:47:50 +0800

    PUSH

--- hr/manage-attendance.php
+++ hr/manage-attendance.php
@@ -17,12 +17,27 @@ $tlate  = 0 ;
 $tundertime  = 0 ;
 $tovertime  = 0 ;
 
+$startdate = "";
+$enddate = "";
+
 if(isset($_GET['teacherid'])){
 
     $teacherid = $_GET['teacherid'];
     $startdate = $_GET['startdate'];
     $enddate = $_GET['enddate'];
-    
+
+    if(!empty($_GET['teacherid']))
+    {
+        $query ="SELECT * FROM time_attendance WHERE teacher_id = $teacherid AND YEAR(expected_timein) = YEAR(NOW())";
+        $query_run=mysqli_query($conn, $query);
+        if(mysqli_num_rows($query_run) > 0) 
+        {
+            
+        }else{
+            $query ="CALL fillattendance( $teacherid , DATE(CONCAT(YEAR(CURDATE()),'-1-1')),DATE(CONCAT(YEAR(CURDATE()),'-12-31'))); ";
+            $query_run=mysqli_query($conn, $query);
+        }
+    }
     
     $query ="SELECT * FROM teacher a INNER JOIN department b ON a.department_id = b.department_id WHERE teacher_id = $teacherid LIMIT 1";
     $query_run=mysqli_query($conn, $query);
@@ -37,51 +52,73 @@ if(isset($_GET['teacherid'])){
             $tdepartment = $row['department_name'];
             $tspecialization = $row['specialization'];
         }
-    }
-
-    $paramaters = " WHERE status='work' AND a.teacher_id = $teacherid AND DATE(a.expected_timein) >= DATE('$startdate') AND DATE(a.expected_timeout) <= DATE('$enddate')"; 
-
-    $query ="SELECT * FROM time_attendance a LEFT JOIN teacher b ON a.teacher_id = b.teacher_id $paramaters ORDER BY a.teacher_id DESC";
-    $query_run=mysqli_query($conn, $query);
-    if(mysqli_num_rows($query_run) > 0) 
-    {
-        while($row=mysqli_fetch_assoc($query_run))
-        {
-
-            $expecteddate = strtotime($row['expected_timein']);
-            $actualdate   = strtotime($row['actual_timein']);
-            if($actualdate >= $expecteddate){
-                $late = round(abs ($actualdate - $expecteddate) / 3600,2) ; 
-                $tlate = $tlate + $late ; 
-            }
-
-
-            $expecteddate = strtotime($row['expected_timeout']);
-            $actualdate   = strtotime($row['actual_timeout']);
-            if($actualdate <= $expecteddate){
-                $undertime = round(abs ($actualdate - $expecteddate) / 3600,2) ; 
-               $tundertime = $tundertime + $undertime ; 
-
-            }
-
-             $expecteddate = strtotime($row['expected_timeout']);
-             $actualdate   = strtotime($row['actual_timeout']);
-             if($actualdate >= $expecteddate){
-                 $overtime = round(abs ($actualdate - $expecteddate) / 3600,2) ; 
-                $tovertime = $tovertime + $overtime ; 
-
-             }
-
-             $actualstartdate   = strtotime($row['actual_timein']);
-             $actualenddate   = strtotime($row['actual_timeout']);
-             $workhrs = round(abs ($actualstartdate - $actualenddate) / 3600,2) ; 
-             $totalwork = $totalwork +  $workhrs;
-
+        if((empty($startdate)) && (empty($enddate))){
+            $paramaters = " WHERE  a.teacher_id = $teacherid "; 
+        }else{
+            $paramaters = " WHERE  a.teacher_id = $teacherid AND DATE(a.expected_timein) >= DATE('$startdate') AND DATE(a.expected_timeout) <= DATE('$enddate')"; 
 
         }
-            $totalwork = $totalwork  - $tovertime ; 
-         
+        
+        
+        $paramaters2 = " WHERE status='work' AND a.teacher_id = $teacherid AND DATE(a.expected_timein) >= DATE('$startdate') AND DATE(a.expected_timeout) <= DATE('$enddate')"; 
+
+        $query ="SELECT * FROM time_attendance a LEFT JOIN teacher b ON a.teacher_id = b.teacher_id $paramaters2 ORDER BY a.teacher_id DESC";
+        $query_run=mysqli_query($conn, $query);
+        if(mysqli_num_rows($query_run) > 0) 
+        {
+            while($row=mysqli_fetch_assoc($query_run))
+            {
+
+                $expecteddate = strtotime($row['expected_timein']);
+                $actualdate   = strtotime($row['actual_timein']);
+                if($actualdate >= $expecteddate){
+                    $late = round(abs ($actualdate - $expecteddate) / 3600,2) ; 
+                    $tlate = $tlate + $late ; 
+                }
+
+
+                $expecteddate = strtotime($row['expected_timeout']);
+                $actualdate   = strtotime($row['actual_timeout']);
+                if($actualdate <= $expecteddate){
+                    $undertime = round(abs ($actualdate - $expecteddate) / 3600,2) ; 
+                $tundertime = $tundertime + $undertime ; 
+
+                }
+
+                $expecteddate = strtotime($row['expected_timeout']);
+                $actualdate   = strtotime($row['actual_timeout']);
+                if($actualdate >= $expecteddate){
+                    $overtime = round(abs ($actualdate - $expecteddate) / 3600,2) ; 
+                    $tovertime = $tovertime + $overtime ; 
+
+                }
+
+                $actualstartdate   = strtotime($row['actual_timein']);
+                $actualenddate   = strtotime($row['actual_timeout']);
+                $workhrs = round(abs ($actualstartdate - $actualenddate) / 3600,2) ; 
+                $totalwork = $totalwork +  $workhrs;
+
+
+            }
+                $totalwork = $totalwork  - $tovertime ; 
+            
+        }
+
+        
+    }else{
+        echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
+        echo '<script>Swal.fire({
+            title: "Warning",
+            text: "ID does not exist.",
+            icon: "warning",
+            confirmButtonText: "OK"
+        }).then(function() {
+            window.location.href = "manage-departments.php";
+        });</script>';
     }
+
+    
+    
 
 }
 
@@ -140,7 +177,7 @@ if(isset($_GET['teacherid'])){
                                         <label>START DATE:</label>
                                     </div>
                                     <div class="col-8">
-                                     <input type="date" form="srchform" name="startdate" class="form-control " placeholder="" required />
+                                     <input type="date" form="srchform" name="startdate" class="form-control " value="<?php echo $startdate; ?>" placeholder="" required />
                                     </div>
                                 </div>
                                 <div class="row mt-3">
@@ -148,7 +185,7 @@ if(isset($_GET['teacherid'])){
                                         <label>END DATE:</label>
                                     </div>
                                     <div class="col-8">
-                                     <input type="date" form="srchform" name="enddate" class="form-control " placeholder="" required/>
+                                     <input type="date" form="srchform" name="enddate" class="form-control " value="<?php echo $enddate; ?>" placeholder="" required/>
                                     </div>
                                 </div>
 
@@ -193,7 +230,7 @@ if(isset($_GET['teacherid'])){
 
                                     <div class="col-3" >
 
-                                        <img src="<?php echo $tlocation ; ?>" alt="alternative" width="100" height="100"/> 
+                                        <img src="<?php if(empty($tlocation)){ echo $tlocation ; }else{ echo "../uploads/teacher.png"; } ?>" alt="alternative" width="100" height="100"/> 
                                     </div>                             
 
                                     <div class="col-12 mt-2 ">
@@ -268,7 +305,7 @@ if(isset($_GET['teacherid'])){
                         <td><?php 
                          $expecteddate = strtotime($row['expected_timein']);
                          $actualdate   = strtotime($row['actual_timein']);
-                         if($actualdate >= $expecteddate){
+                         if($actualdate >= $expecteddate && ($actualdate > 0)  ){
                             echo $late = round(abs ($actualdate - $expecteddate) / 3600,2) ."hr"; 
                          }else{
                             echo "0hr";
@@ -277,17 +314,17 @@ if(isset($_GET['teacherid'])){
                         <td><?php 
                          $expecteddate = strtotime($row['expected_timeout']);
                          $actualdate   = strtotime($row['actual_timeout']);
-                         if($actualdate <= $expecteddate){
+                         if(($actualdate <= $expecteddate) && ($actualdate > 0)  ){
                             echo $undertime = round(abs ($actualdate - $expecteddate) / 3600,2) ."hr"; 
                          }else{
-                            echo "0";
+                            echo "0hr";
                          }
                         
                         ?></td>
                         <td><?php 
                           $expecteddate = strtotime($row['expected_timeout']);
-                          $actualdate   = strtotime($row['actual_timeout']);
-                          if($actualdate >= $expecteddate){
+                          $actualdate   = strtotime($row['actual_timeout'] );
+                          if($actualdate >= $expecteddate && ($actualdate > 0) ){
                              echo $overtime = round(abs ($actualdate - $expecteddate) / 3600,2) ."hr"; 
                           }else{
                              echo "0hr";
@@ -300,7 +337,7 @@ if(isset($_GET['teacherid'])){
 <!-- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
                         <td>
                             <!--Edit Pop Up Modal -->
-                            <div class="modal fade" id="editManageDepartments" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal fade" id="edit<?php echo $row['ta_id']?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
@@ -310,15 +347,42 @@ if(isset($_GET['teacherid'])){
                                             </button>
                                         </div>
 
-                                    <form action="manage-departments-function.php" method = "POST"> 
+                                    <form action="manage-attendance-func.php" method = "POST"> 
 
                                         <div class="modal-body">
+                                                <input type="text" name= "teacherid" id ="" value="<?php echo $row['teacher_id']; ?>">
 
-                                                <input type="hidden" name= "edit_ID" id ="edit_ID">
+                                                 <input type="text" name= "startdate" id ="" value="<?php echo $startdate; ?>">
+                                                 <input type="text" name= "enddate" id ="" value="<?php echo $enddate; ?>">
 
+                                                <input type="text" name= "edit_ID" id ="" value="<?php echo $row['ta_id']; ?>">
                                                 <div class="form-group">
-                                                    <label for="#">Department Name</label>
-                                                    <input type="text" class="form-control" id="edit_Department_Name" name="department_name" required>
+                                                    <label for="#">Teacher Name</label>
+                                                    <input type="text" class="form-control" id="" name="" readonly value="<?php echo $row['firstname'] . " " . $row['lastname'] ; ?>">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="#workstatus">Status</label>
+                                                    <select class="form-control" id="workstatus" name="workstatus" required>
+                                                        <option disabled selected> Select Status </option>
+                                                        <option  value="work" <?php if($row['status']=="work"){ echo "selected"; }?> >Work</option>                     
+                                                        <option  value="restday" <?php if($row['status']=="restday"){ echo "selected"; }?>>Rest Day</option>
+                                                    </select>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="#">Expected Time-In</label>
+                                                    <input type="datetime-local" class="form-control" id="" name="exp_timein"  value="<?php echo $row['expected_timein']?>">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="#">Expected Time-Out</label>
+                                                    <input type="datetime-local" class="form-control" id="" name="exp_timeout"  value="<?php echo $row['expected_timeout']?>">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="#">Acutal Time-In</label>
+                                                    <input type="datetime-local" class="form-control" id="" name="act_timein"  value="<?php echo $row['actual_timein']?>">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="#">Acutal Time-Out</label>
+                                                    <input type="datetime-local" class="form-control" id="" name="act_timeout"  value="<?php echo $row['actual_timeout']?>">
                                                 </div>
 
                                         </div>
@@ -332,7 +396,7 @@ if(isset($_GET['teacherid'])){
                                 </div>
                             </div>  
 
-                            <button type="button" class="btn btn-success edit_btn" data-toggle="modal" data-target="#editManageDepartments " >Edit</button>
+                            <button type="button" class="btn btn-success edit_btn" data-toggle="modal" data-target="#edit<?php echo $row['ta_id']?>" >Edit</button>
                         </td>
 
 
