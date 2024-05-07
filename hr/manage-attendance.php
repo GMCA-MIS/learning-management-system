709a72956ee35a8d6ee203af5d4604a3537df4af
Red Star <80256611+redcaloocan@users.noreply.github.com>
2024-05-08 06:24:35 +0800

    Update manage-attendance.php

--- hr/manage-attendance.php
+++ hr/manage-attendance.php
@@ -452,7 +452,7 @@ if(isset($_GET['teacherid'])){
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">USER QR CODE</h5>
+                <h5 class="modal-title" id="exampleModalLabel">TEACHER QR CODE</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
